ebe1bee62d19c30f9be7be3ce5fc76da425315d1
island <1012812908@qq.com>
2025-09-07 01:54:04 +0800

    Update: sort

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,15 +3,17 @@ import { defineConfig } from 'vitepress'
 
 // 自动获取 posts 目录下的 MD 文件
 const getPosts = (dir = 'posts') => {
-  return glob.sync(`docs/${dir}/**/*.md`).map(file => {
+  const posts = glob.sync(`docs/${dir}/**/*.md`).map((file) => {
     const path = file.replace('docs/', '').replace('.md', '')
     const [date, slug] = path.split('/').slice(-2)
-    return { text: slug, link: path }
+    const sortIndex = +slug.match(/\d+/)?.[0] || new Date()
+    return { text: slug, link: path, date, sortIndex }
   })
+  return posts.sort((a, b) => a.sortIndex - b.sortIndex)
 }
 
 const dailyPosts = getPosts('daily')
-
+console.log(JSON.stringify(dailyPosts))
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -46,5 +48,5 @@ export default defineConfig({
     }
   },
   outDir: '../public',
-  ignoreDeadLinks:true
+  ignoreDeadLinks: true
 })
